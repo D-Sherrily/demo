d27c3cf7d1db673a095b69ee1fa83cb0a22eeb5a
D-sherrily <1132917088@qq.com>
2020-06-24 16:59:17 +0800

    Initial commit

---
+++ src/test/java/com/you/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.you.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
